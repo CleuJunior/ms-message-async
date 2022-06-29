3760b54d6822593bbd90dc41255135f6a23ab4b4
Cleonildo Jr <cleu.junior@gmail.com>
2022-06-28 22:36:36 -0300

    first commit

---
+++ src/main/java/com/br/cleonildo/MsMessageAsyncApplication.java
@@ -0,0 +1,13 @@
+package com.br.cleonildo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMessageAsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMessageAsyncApplication.class, args);
+	}
+
+}
